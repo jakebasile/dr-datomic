34ef2bf9fc25b8adb34d6d617f7979ebb8929b2a
Jake Basile <jake@jakebasile.com>
2015-10-12 14:17:05 -0500

    add repl ns for convenience

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject jakebasile/dr-datomic "0.0.1"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.datomic/datomic-free "0.9.5302" :exclusions [joda-time]]]
-  :repl-options {:init (do (require '[datomic.api :as d]))
-                 :init-ns jakebasile.dr-datomic.intro})
+  :repl-options {:init-ns jakebasile.dr-datomic.repl})
 
---
+++ src/jakebasile/dr_datomic/repl.clj
@@ -0,0 +1,6 @@
+(ns jakebasile.dr-datomic.repl
+  (:require [datomic.api :as d]
+            [jakebasile.dr-datomic
+             [intro :as intro]
+             [refs :as refs]]))
+
